9be73886c93be3558d5e7f10923ee678f6c5271a
kmelillo <kmelillo@dotheyknow.net>
2019-11-15 09:54:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  if(file_exists('login.php')) include 'login.php';
+  $SiteTitle = "Lookup Tools";
+  $PageTitle = "Main Menu";
+  include 'header.php';
+  include 'main.php';
+  include 'footer.php';
+?>
