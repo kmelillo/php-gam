881a2edb51bb2e3f8c8138c83502741ae4cff6b2
kmelillo <kmelillo@dotheyknow.net>
2019-11-15 09:52:56 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+$SiteTitle = "GAM Lookup Tools";
+echo '<html><head><meta name="google" content="notranslate">';
+echo "<title>$SiteTitle | $PageTitle</title></head><body>";
+echo "<h1><center>$SiteTitle | $PageTitle</center></h1>";
+echo "<hr style='border: 10px solid DarkBlue; border-radius: 5px;'>";
+?>
